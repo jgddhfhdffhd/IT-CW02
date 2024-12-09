21ffef7e9d28fb093edcc9ccf1c371271c9c14ae
Yue Shen <b01734472@studentmail.uws.ac.uk>
2024-12-10 01:15:50 +0800

    frontend

---
+++ Frontend/scripts.js
@@ -0,0 +1,5 @@
+// Example: Add interactivity to the search button
+document.querySelector('.btn-primary').addEventListener('click', function (event) {
+  event.preventDefault();
+  alert('Search functionality is not implemented yet!');
+});
